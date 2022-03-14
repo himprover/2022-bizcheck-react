868162116b2d1eda27648af817ed3f62fa06ac1d
himprover <hwang.improver@gmail.com>
2022-03-14 14:45:45 +0900

    feat: add Pretendard font

--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -1,6 +1,13 @@
 import {createGlobalStyle} from 'styled-components';
 import {normalize} from 'styled-normalize';
 
+import {font} from 'styles/font';
+
 export const GlobalStyle = createGlobalStyle`
     ${normalize}
+    ${font}
+
+    * {
+        font-family: 'Pretendard';
+    }
 `;
