c6cc8ef492712e52c6cdbb56def7eea308f755c9
himprover <hwang.improver@gmail.com>
2022-03-15 03:18:22 +0900

    refactor: font color white

--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -9,9 +9,13 @@ export const GlobalStyle = createGlobalStyle`
 
     * {
         font-family: 'Pretendard';
+        color:rgb(255,255,255);
     }
-
     html {
         font-size:62.5%;
+    }
+
+    h1, h2, h3, h4 ,h5, h6{
+        margin:0;
     }
 `;
