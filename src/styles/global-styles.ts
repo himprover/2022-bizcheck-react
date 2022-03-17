563a115ca9f9bfe7c5c884eaaf45b4c258c1115e
himprover <hwang.improver@gmail.com>
2022-03-18 01:03:37 +0900

    feat: transition all 0.1s (test)

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -10,9 +10,13 @@ export const GlobalStyle = createGlobalStyle`
     * {
         font-family: 'Pretendard';
         color:rgb(255,255,255);
+        transition: all 0.1s ease-in-out;
     }
     html {
         font-size:62.5%;
+    }
+    body{
+        overflow:hidden;
     }
 
     h1, h2, h3, h4 ,h5, h6{
